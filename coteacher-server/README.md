e5e4a9bf5773c795f98501cee7b024d09cdba129
KinjiKawaguchi <kawakin0310@icloud.com>
2023-11-27 15:43:05 +0900

    feat (api): Add database connection and route for checking account existence

--- coteacher-server/README.md
+++ coteacher-server/README.md
@@ -1,1 +1,4 @@
+# 参考
+
 https://cloud.google.com/run/docs/configuring/services/containers?hl=ja#command-line
+https://zenn.dev/jinwatanabe/articles/ae0d75444fa056
--- coteacher-server/main.go
+++ coteacher-server/main.go
@@ -1,24 +1,60 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+var db *sql.DB
+
+type Student struct {
+	StudentID int64
+	Email     string
+	Name      string
+}
+
 func main() {
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Cloud Run以外でのデフォルトポート
+	// Load in the `.env` file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env", err)
 	}
 
-	r.Run(":" + port)
+	// Open a connection to the database
+	db, err = sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatal("failed to open db connection", err)
+	}
+
+	// Build router & define routes
+	router := gin.Default()
+	router.GET("/CheckAcountExist/:email", CheckAcountExist)
+
+	// Run the router
+	router.Run()
+}
+
+func CheckAcountExist(c *gin.Context) {
+	email := c.Param("email")
+	email = strings.ReplaceAll(email, "/", "")
+
+	// No need to convert email to an integer.
+	var student Student
+	query := `SELECT * FROM Student WHERE email = ?`
+	err := db.QueryRow(query, email).Scan(&student.StudentID, &student.Email, &student.Name)
+	if err != nil {
+		c.JSON(404, gin.H{
+			"exist": false,
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"exist": true,
+		})
+	}
 }
