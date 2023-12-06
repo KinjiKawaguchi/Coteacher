fb4987a2346d875cb80c4651e49e0e647b280767
KinjiKawaguchi <kawakin0310@icloud.com>
2023-12-06 20:50:56 +0900

    refactor: Add GET request for getting participating class

--- coteacher-app/src/libs/services/api/index.ts
+++ coteacher-app/src/libs/services/api/index.ts
@@ -1,7 +1,10 @@
 async function checkStudentExist(email: string) {
   // const API_URL = process.env.API_URL!;
   const response = await fetch(
-    `https://api-image-pgfe7sqiia-an.a.run.app/Student/CheckAcountExist/${email}`
+    `https://api-image-pgfe7sqiia-an.a.run.app/Student/CheckAcountExist/${email}`,
+    {
+      method: 'GET',
+    }
   );
   const data = await response.json();
   console.log(data.exist);
@@ -12,8 +15,9 @@ async function createStudent(name: string) {
   const email = window.localStorage.getItem('email');
   // POSTリクエストに変更
   const response = await fetch(
-    `https://api-image-pgfe7sqiia-an.a.run.app/Student/Create?Email=${email}&Name=${name}`, {
-      method: 'POST', // POSTメソッドを指定
+    `https://api-image-pgfe7sqiia-an.a.run.app/Student/Create?Email=${email}&Name=${name}`,
+    {
+      method: 'POST',
     }
   );
 
@@ -32,5 +36,29 @@ async function createStudent(name: string) {
   }
 }
 
+async function getParticipatingClass() {
+  // const StudentID = window.localStorage.getItem('StudentID');
+  const StudentID = 'e08397af-2d13-4814-b671-9831bb2e395b';
 
-export { checkStudentExist, createStudent };
+  const response = await fetch(
+    `https://api-image-pgfe7sqiia-an.a.run.app/StudentClass/GetParticipatingClass?StudentID=${StudentID}`,
+    {
+      method: 'GET',
+    }
+  );
+
+  if (!response.ok) {
+    console.error('Response error:', response.status);
+    return;
+  }
+
+  try {
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+  }
+}
+
+export { checkStudentExist, createStudent, getParticipatingClass };
